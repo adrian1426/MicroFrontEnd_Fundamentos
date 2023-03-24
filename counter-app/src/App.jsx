b784d3b7f516f5a46c49c12b7626fc07afa81770
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-24 17:24:11 -0600

    creando nuevo microfront counter, exponiendo y consumiendo

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from 'navbar/Navbar';
-import Counter from 'counter/Counter';
+import Counter from "./components/Counter";
 
 const App = () => (
-  <div>
-    <Navbar name="Adrian" />
-    Host app
-
-    <hr />
+  <div >
     <Counter />
   </div>
 );
--- counter-app/webpack.config.js
+++ counter-app/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "http://localhost:3002/",
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 3000,
+    port: 3002,
     historyApiFallback: true,
   },
 
@@ -41,13 +41,12 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "host_app",
+      name: "counter_app",
       filename: "remoteEntry.js",
-      remotes: {
-        navbar: "navbar_app@http://localhost:3001/remoteEntry.js",
-        counter: "counter_app@http://localhost:3002/remoteEntry.js"
+      remotes: {},
+      exposes: {
+        "./Counter": "./src/components/Counter.jsx"
       },
-      exposes: {},
       shared: {
         ...deps,
         react: {
