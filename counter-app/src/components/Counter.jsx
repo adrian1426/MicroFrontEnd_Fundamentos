e3de76f75d8283363c5179f983385eb9ac80766d
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-25 18:37:50 -0600

    estilos dinamicos

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import Button from './Button';
+import Styled from '@emotion/styled';
 import './Counter.css';
+
+const Error = Styled.span`
+  color: red;
+`;
+
+const Success = Styled.span`
+  color: green;
+`;
 
 const Counter = () => {
   const [counter, setCounter] = useState(5);
 
   return (
     <div className="counter-container">
-      <h2>Counter: {counter}</h2>
+      <h2>Counter: {counter < 0 ? <Error>{counter}</Error> : <Success>{counter}</Success>}</h2>
       <br />
       <Button onClick={() => setCounter(counter + 1)}>+1</Button>
       <Button onClick={() => setCounter(counter - 1)}>-1</Button>
