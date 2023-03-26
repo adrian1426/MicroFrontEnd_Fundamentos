717b881e5f7e8b7aeb6d2e99cd6fdfaa5952f619
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-25 18:27:17 -0600

    agregando styled component a button

--- counter-app/src/components/Button.jsx
+++ counter-app/src/components/Button.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Styled from '@emotion/styled';
+
+const ButtonStyled = Styled.button`
+  cursor: pointer;
+  font-size: 1.3rem;
+  margin: 0px 0.5rem;
+`;
 
 const Button = (props) => {
   const { children, ...rest } = props;
 
   return (
-    <button {...rest}>
+    <ButtonStyled {...rest}>
       {children}
-    </button>
+    </ButtonStyled>
   );
 };
 
